158268119de4372e9343e2ca83b371ddd3ffa498
Hiroshi Omata <op.homata@gmail.com>
2020-08-29 10:28:15 +0900

    first commit

---
+++ config/urls.py
@@ -0,0 +1,43 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, path
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.i18n import JavaScriptCatalog
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+    path('favicon\.ico', RedirectView.as_view(url='/static/images/favicon.ico')),
+
+    # apps (detection)
+    path('', include('apps.urls')),
+
+    # apis
+    path('api/v1.0/', include('apis.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
